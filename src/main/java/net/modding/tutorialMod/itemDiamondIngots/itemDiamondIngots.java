d565a067bb91400de83ea697316ba5546978a7c4
YongYiChen <NatusVincere850@gmail.com>
2022-07-12 12:57:46 +0800

    I added itemDiamondIngots.java, it's a class about new items.

---
+++ src/main/java/net/modding/tutorialMod/itemDiamondIngots/itemDiamondIngots.java
@@ -0,0 +1,21 @@
+package net.modding.tutorialMod.itemDiamondIngots;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.modding.tutorialMod.TutorialMod;
+
+public class itemDiamondIngots {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> itemDiamondIngots =
+            ITEMS.register("itemDiamondIngots", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
